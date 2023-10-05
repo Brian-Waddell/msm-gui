9bc390225a1bd75abab927cb175a2caa03a4c21d
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-05 18:02:59 +0000

    add destory link for actor

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  post("/insert_actor", {:controller => "actors", :action => "create"})
+  get("/delete_actor/:actor_id", {:controller => "actors", :action => "destroy"})
 end
